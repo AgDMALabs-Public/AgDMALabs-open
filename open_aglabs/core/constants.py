50dd6d9e3887445db144e5543b853a9185121d1f
agdmalabs <d.a.williams11@gmail.com>
2025-11-10 11:28:39 -0800

    Update constants.py

--- open_aglabs/core/constants.py
+++ open_aglabs/core/constants.py
@@ -6,16 +6,16 @@ IMAGE_TYPE_LIST = ['original', 'annotation', 'augmented', 'synthetic']
 
 ORIENTATION_LIST = ['landscape', 'portrait']
 
-DEVICE_LIST = ['mobile', 'auxiliary_camera', 'drone', 'amiga', 'rafiki']
+DEVICE_LIST = ['mobile', 'auxiliary_camera', 'drone', 'amiga', 'rafiki', 'truckee', 'unknown']
 
 CROP_LIST = ["barley", "maize", "pearl_millet", "finger_millet", "rice", "sorghum", "wheat", "bush_bean",
              "climbing_bean", "chickpea", 'cowpea', "faba_bean", "grass_pea", "ground_nut", "lentil", "pigeonpea",
-             "soybean", "banana", "cassava", "potato", "sweet_potato", "yam", "taro", "corn", "sugarcane"]
+             "soybean", "banana", "cassava", "potato", "sweet_potato", "yam", "taro", "corn", "sugarcane", 'unknown']
 
 YEAR_LIST = np.arange(1900, 2050, 1)
 
 TIME_OF_YEAR_LIST = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'fall', 'winter', 'spring',
-                     'summer', 'short_rains', 'long_rains']
+                     'summer', 'short_rains', 'long_rains', 'unknown']
 
 SOIL_COLOR = ["light", "dark", "red"]
 
@@ -24,6 +24,7 @@ RATE_UNITS = ("kg/ha", 'lbs/ac', 'g/m2', 'oz/ft2', 'l/ha', 'ml/m2', 'gal/ac')
 AMOUNT_UNITS = ["Kg", "lbs", "g", "oz", "l", "ml", "gal"]
 
 COUNTRY_CODES = {
+    'UNK': "unknown",
     "AFG": "Afghanistan",
     "ALB": "Albania",
     "DZA": "Algeria",
