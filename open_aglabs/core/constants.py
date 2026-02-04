1b08eb4e1bdd6366bf9e757847198cf4992206e7
agdmalabs <d.a.williams11@gmail.com>
2026-02-04 13:28:59 -0800

    Update constants.py

--- open_aglabs/core/constants.py
+++ open_aglabs/core/constants.py
@@ -11,7 +11,8 @@ DEVICE_LIST = ['mobile', 'auxiliary_camera', 'drone', 'amiga', 'rafiki', 'trucke
 CROP_LIST = ["barley", "maize", "pearl_millet", "finger_millet", "rice", "sorghum", "wheat", "bush_bean",
              "climbing_bean", "common_bean", "chickpea", 'cowpea', "faba_bean", "grass_pea",
              "groundnut", "lentil", "pigeonpea", "soybean", "banana", "cassava", "potato",
-             "sweet_potato", "yam", "taro", "corn", "sugarcane", 'unknown', 'legumes']
+             "sweet_potato", "yam", "taro", "corn", "sugarcane", 'unknown', 'legumes', 'tomato', 'turmeric', 'sesame',
+             'onion']
 
 YEAR_LIST = np.arange(1900, 2050, 1)
 
